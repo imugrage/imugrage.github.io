3a1a23d78407b4e056e3bb6acd619a8847b9eb6a
Isaiah Mugrage <isaiah.mugrage@gmail.com>
2022-03-09 20:12:10 -0500

    add drive

--- bento/config.js
+++ bento/config.js
@@ -53,9 +53,9 @@ const CONFIG = {
   firstButtonsContainer: [
     {
       id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      name: 'Drive',
+      icon: 'drive',
+      link: 'https://drive.google.com/',
     },
     {
       id: '2',
