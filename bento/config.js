4ff30a5754e0240cebc4e7e057d2858ecfc37882
imugrage <98566331+imugrage@users.noreply.github.com>
2022-04-10 19:27:34 -0400

    reverting

--- bento/config.js
+++ bento/config.js
@@ -49,7 +49,6 @@ const CONFIG = {
   // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
   // ├┴┐│ │ │  │ │ ││││└─┐
   // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
-// Buttons are not used in current mode 4/10/22
 
   firstButtonsContainer: [
     {
